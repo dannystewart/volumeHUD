a0661583b46eb690cd4b8e5ae182568de7012819
Danny Stewart <danny@stewart.cc>
2025-09-21 08:23:00 -0400

    Initial Commit

---
+++ Volume HUD/Volume_HUDApp.swift
@@ -0,0 +1,17 @@
+//
+//  Volume_HUDApp.swift
+//  Volume HUD
+//
+//  Created by Danny Stewart on 9/21/25.
+//
+
+import SwiftUI
+
+@main
+struct Volume_HUDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
