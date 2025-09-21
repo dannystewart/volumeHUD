adc9e318e90a82add87032118137604a63537662
Danny Stewart <danny@stewart.cc>
2025-09-21 17:59:27 -0400

    chore(scripts): remove build scripts
    Removes development scripts for building and running the volumeHUD application and adds scripts directory to gitignore to prevent future script files from being tracked in version control. Those were mostly just for me and aren't the best way to build the app.

--- scripts/run-swift.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")" && cd .. || exit 1
-pkill -f 'volumeHUD' || true
-sleep 0.5
-swift build
-echo 'Starting volumeHUD...'
-exec ./.build/arm64-apple-macosx/debug/volumeHUD
